35313cd64aaa84fe4ebb8f628d2d52d5c74beb99
Renato Fagalde <renato@likwi.com.br>
2022-01-09 20:49:10 -0300

    Log4j2 Setup

--- src/main/java/br/com/likwi/photoapp/aws/GetUserHandler.java
+++ src/main/java/br/com/likwi/photoapp/aws/GetUserHandler.java
@@ -3,10 +3,16 @@ package br.com.likwi.photoapp.aws;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class GetUserHandler implements RequestHandler {
+import java.util.Map;
+
+@
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Override
-    public Object handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        final Map<String, String> pathParameters = input.getPathParameters();
+
         return null;
     }
 }
