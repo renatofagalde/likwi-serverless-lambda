416f6794ce5a745d158acaa6a3d2df1e6ef2cf5c
Renato Fagalde <renato@likwi.com.br>
2022-01-10 09:26:03 -0300

    Using ProxyRequest

--- src/main/java/br/com/likwi/photoapp/aws/GetUserHandler.java
+++ src/main/java/br/com/likwi/photoapp/aws/GetUserHandler.java
@@ -12,6 +12,7 @@ import java.text.MessageFormat;
 import java.util.Map;
 import java.util.UUID;
 
+
 public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private static Logger logger = LoggerFactory.getLogger(GetUserHandler.class);
@@ -20,15 +21,15 @@ public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEven
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
         final Map<String, String> pathParameters = input.getPathParameters();
         final String userId = pathParameters.get("userId");
-        logger.info(MessageFormat.format("User ID = {0}", userId));
+        logger.info(MessageFormat.format("new customer registration {0}", userId));
 
         final JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("firstName","Ronaldo");
         jsonObject.addProperty("lastName","Nazário");
-        jsonObject.addProperty("id", UUID.randomUUID().toString());
+        jsonObject.addProperty("id", userId.concat("___").concat(UUID.randomUUID().toString()));
 
         final APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-        response.withStatusCode(200).withBody(jsonObject.toString())
+        response.withStatusCode(200).withBody(jsonObject.toString());
 
         return response;
     }
